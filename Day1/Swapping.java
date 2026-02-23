4572e25cd23c66b8593470ea15b6d7c85ca9bb24
mudili siva <mudhilisiva@gmail.com>
2026-02-23 12:22:45 +0530

    Day1 problem solved

---
+++ Day1/Swapping.java
@@ -0,0 +1,14 @@
+package Day1;
+
+public class Swapping {
+    public static void main(String[] args) {
+        int a=20; 
+        int b=30;
+        System.out.println("Before swapping a="+a+" b="+b);
+        int temp=a;
+        a=b;
+        b=temp;
+        System.out.println("After swapping a="+a+" b="+b);
+
+    }
+}
