0317842759996ea46384495ab68d336553f15af0
mudili siva <mudhilisiva@gmail.com>
2026-02-23 09:04:19 +0530

    Initial commit

---
+++ AddNumbers.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class AddNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a value");
+        int a = sc.nextInt();
+        System.out.println( "Enter b value");
+        int b= sc.nextInt();
+        int result=a+b;
+        System.out.println("SUM is "+result);
+        sc.close();
+    }
+    
+}
