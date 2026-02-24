14db7d6cee331306d069ee181f54f7cee6efa77e
mudili siva <mudhilisiva@gmail.com>
2026-02-24 12:14:28 +0530

    Day1 problem solved

--- Day1/HelloWorld.java
+++ Day1/HelloWorld.java
@@ -2,6 +2,6 @@ package Day1;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, this is my first Java Program!");
+        System.out.println("Hello Everyone, this is my first Java Program!");
     }
 }
